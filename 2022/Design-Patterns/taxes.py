e5b5b202cb947ec6250f3fc3283ec807ace98337
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2022-03-02 22:16:58 -0300

    Implementing abstract methods in taxes.py

--- 2022/Design-Patterns/taxes.py
+++ 2022/Design-Patterns/taxes.py
@@ -48,6 +48,15 @@ class ICPP(Conditional_tax_template):
         else:
             return budget.value * 0.05
 
+    def must_use_taxation(budget):
+        pass
+
+    def max_taxation(budget):
+        pass
+
+    def min_taxation(budget):
+        pass
+
 
 class IKCV(Conditional_tax_template):
 
@@ -57,6 +66,15 @@ class IKCV(Conditional_tax_template):
             return budget.value * 0.1
         else:
             return budget.value * 0.06
+
+    def must_use_taxation(budget):
+        pass
+
+    def max_taxation(budget):
+        pass
+
+    def min_taxation(budget):
+        pass
 
     def __has_item_bigger_than_100_dollars(self, budget):
 

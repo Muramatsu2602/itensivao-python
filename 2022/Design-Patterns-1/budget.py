8f2220a15132c1f07cd8a74a475359c131c0ba36
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2022-03-06 00:27:34 -0300

    Creating classes for each budget state

--- 2022/Design-Patterns-1/budget.py
+++ 2022/Design-Patterns-1/budget.py
@@ -1,3 +1,89 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Budget_of_a_state(object):
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def apply_extra_discount(self, budget):
+        pass
+
+    @abstractmethod
+    def approve(self, budget):
+        pass
+
+    @abstractmethod
+    def deny(self, budget):
+        pass
+
+    @abstractmethod
+    def end(self, budget):
+        pass
+
+
+class In_approval(Budget_of_a_state):
+
+    def apply_extra_discount(self, budget):
+        budget.add_extra_discount(budget.value * 0.02)
+
+    def approve(self, budget):
+        budget.current_state = Approval()
+
+    def deny(self, budget):
+        budget.current_state = Denied()
+
+    def end(self, budget):
+        raise Exception('A budget under approval cannot go to ended')
+
+
+class Approval(Budget_of_a_state):
+
+    def apply_extra_discount(self, budget):
+        budget.add_extra_discount(budget.value * 0.05)
+
+    def approve(self, budget):
+        raise Exception('Budget has been already approved')
+
+    def deny(self, budget):
+        raise Exception('Approved Budgets cannot be denied')
+
+    def end(self, budget):
+        budget.current_state = Ended()
+
+
+class Denied(Budget_of_a_state):
+
+    def apply_extra_discount(self, budget):
+        raise Exception(
+            "Denied budgets shall not receive any additional discounts")
+
+    def approve(self, budget):
+        raise Exception('Denied Budget cannot be approved')
+
+    def deny(self, budget):
+        raise Exception('Denied Budget cannot be denied twice')
+
+    def end(self, budget):
+        budget.current_state = Ended()
+
+
+class Ended(Budget_of_a_state):
+
+    def apply_extra_discount(self, budget):
+        raise Exception(
+            "Ended budgets shall not receive any additional discounts")
+
+    def approve(self, budget):
+        raise Exception('Ended Budgets cannot be approved')
+
+    def deny(self, budget):
+        raise Exception('Ended Budgets cannot be denied')
+
+    def end(self, budget):
+        raise Exception('Ended Budgets cannot be ended twice')
+
+
 class Budget(object):
 
     IN_APPROVAL = 1
@@ -7,21 +93,12 @@ class Budget(object):
 
     def __init__(self):  # __ private
         self.__items = []
-        self.current_state = Budget.IN_APPROVAL
+        self.current_state = In_approval()
         self.__extra_discount = 0
 
-    def apply_extra_discount(self):
-        if self.current_state == Budget.IN_APPROVAL:
-            self.__extra_discount += self.value * 0.02
-        
-        elif self.current_state == Budget.APPROVED:
-            self.__extra_discount += self.value * 0.05
-        
-        elif self.__extra_discount == Budget.DENIED:
-            raise Exception("Denied budgets shall not receive any additional discounts")
-        
-        elif self.__extra_discount == Budget.ENDED:
-            raise Exception("Ended budgets shall not receive any additional discounts")
+    @property
+    def num_items(self):
+        return len(self.__items)
 
     @property
     def value(self):
@@ -29,15 +106,25 @@ class Budget(object):
         for item in self.__items:
             total += item.value
 
-        return total
+        return total - self.__extra_discount
+
+    def approve(self):
+        self.current_state.approve(budget)
+
+    def deny(self):
+        self.current_state.deny(budget)
+
+    def end(self):
+        self.current_state.end(budget)
+
+    def apply_extra_discount(self):
+        self.current_state.apply_extra_discount(self)
+
+    def add_extra_discount(self, discount):
+        self.__extra_discount += discount
 
     def get_items(self):
-
         return tuple(self.__items)
-
-    @property
-    def num_items(self):
-        return len(self.__items)
 
     def add_item(self, item):
         self.__items.append(item)
@@ -56,3 +143,17 @@ class Item(object):
     @property
     def name(self):
         return self.__name
+
+    if __name__ == "__main__":
+        from budget import Item
+
+        budget = Budget()
+        budget.add_item(Item('Item 1', 50))
+        budget.add_item(Item('Item 2', 200))
+        budget.add_item(Item('Item 3', 245))
+
+        print(budget.value)
+
+        budget.apply_extra_discount()
+
+        print(budget.value)

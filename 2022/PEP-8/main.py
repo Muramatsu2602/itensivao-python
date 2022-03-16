48d842e473cd67a968203b2832f8705eb93fea7d
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2022-03-15 23:41:06 -0300

    More tests on main.py

--- 2022/PEP-8/main.py
+++ 2022/PEP-8/main.py
@@ -1,16 +1,16 @@
 from normal_queue import NormalQueue
 from priority_queue import PriorityQueue
 
-# test_queue = NormalQueue()
-# test_queue.updateQueue()
-# test_queue.updateQueue()
-# print(test_queue.callCustomer(5))
+test_queue = NormalQueue()
+test_queue.updateQueue()
+test_queue.updateQueue()
+print(test_queue.callCustomer(5))
 
 
-test_queue2 = PriorityQueue()
-test_queue2.update_queue()
-test_queue2.update_queue()
-test_queue2.update_queue()
+# test_queue2 = PriorityQueue()
+# test_queue2.update_queue()
+# test_queue2.update_queue()
+# test_queue2.update_queue()
 
 print(test_queue2.call_customer(10))
 print(test_queue2.call_customer(1))
